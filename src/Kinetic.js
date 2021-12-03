121e759c252a601e9a8323d494cd0bb875c10f51
DArmstrong87 <lenzflare@gmail.com>
2021-12-02 21:53:38 -0600

    set up application views and components

---
+++ src/Kinetic.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./components/nav/NavBar"
+
+
+export const Kinetic = () => {
+    return <>
+        <NavBar />
+        <ApplicationViews />
+    </>
+}
