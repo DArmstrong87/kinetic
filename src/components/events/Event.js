cd575883edc3bdb0c9f213f9edd075e64afb2760
DArmstrong87 <lenzflare@gmail.com>
2021-12-15 11:43:46 -0600

    lots of styling across multiple modules

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { CourseMap } from "./CourseMap";
 import { getAthleteEvent, getEvent, leaveEvent, signUp } from "./EventsProvider";
+import "./Event.css"
 
 export const Event = () => {
     const [event, setEvent] = useState([])
@@ -19,6 +20,7 @@ export const Event = () => {
             }
         }, [eventId]
     )
+
     const eventSignUp = (id) => {
         signUp(id)
             .then(res => {
@@ -29,6 +31,7 @@ export const Event = () => {
                 }
             })
     }
+
     const eventLeave = (id) => {
         leaveEvent(id)
             .then(res => {
@@ -43,63 +46,79 @@ export const Event = () => {
 
     return (
         <>
-            <h1>{event.name}</h1>
-            <img src={event.event_logo} alt="Event logo" />
-            <p>
-                {date} | {event.city}, {event.state}<br />
-                Start time: {time}{time >= 12 ? 'pm' : 'am'}
-            </p>
-            <p>Distance: {event.total_distance?.toFixed(1)}mi</p>
-            <p>Elevation Gain: {event.total_elev_gain}ft</p>
-
-            {event.event_sports?.length > 1 ? <>
-                <h4>Multi-Sport Event</h4>
-                {event.event_sports?.map(es => {
-                    return <>
-                        <div>
-                            {es.sport.name} | Distance: {es.distance}mi | Elevation Gain: {es.elev_gain}ft
+            <article className="event-article">
+                <section className="e-side">
+                    <div className="e-logo">
+                        <img src={event.event_logo} alt="Event logo" />
+                    </div>
+                    {event.event_sports?.map(es => {
+                        return <div className="e-sport">
+                            {es.sport.name}
                         </div>
-                    </>
-                })}
-            </>
-                : ""}
-
-            <p>{event.description}</p>
-
-            {localStorage.getItem("is_athlete") === "true" ?
-                <>
-                    <p>There are {event.spots_remaining} spots remaining!</p>
-                    {event.spots_remaining !== 0 ?
+                    })}
+                    {localStorage.getItem("is_athlete") === "true" ?
                         <>
-                            {athleteEvent?.length === 0 ?
-                                <div>
-                                    <button onClick={() => eventSignUp(event.id)}>
-                                        Sign Up!
-                                    </button>
-                                </div>
-                                :
+                            <p className="e-signup">Spots remaining:<br />{event.spots_remaining}</p>
+                            {event.spots_remaining !== 0 ?
                                 <>
-                                    You are signed up for {event.name}!
-                                    <button onClick={() => eventLeave(event.id)}>
-                                        Leave Event</button>
+                                    <div className="e-signup">
+                                        {athleteEvent?.length === 0 ?
+                                            <button onClick={() => eventSignUp(event.id)}>
+                                                Sign Up!
+                                            </button>
+                                            :
+                                            <>
+
+                                                You are signed up for<br />{event.name}!<br />
+                                                <button onClick={() => eventLeave(event.id)}>
+                                                    Leave Event</button>
+                                            </>
+                                        }
+                                    </div>
                                 </>
+                                :
+                                "Event registration closed. Maximum participants met."
                             }
                         </>
-                        :
-                        "Event registration closed. Maximum participants met."
-                    }
-                </>
-                : ""}
+                        : ""}
+                </section>
+                
+                <section className="e-details">
+                    <h1>{event.name}</h1>
+                    <p>
+                        {date} | {event.city}, {event.state}<br />
+                        Start time: {time}{time >= 12 ? 'pm' : 'am'}
+                    </p>
+                    <p>Distance: {event.total_distance?.toFixed(1).toLocaleString()}mi || 
+                    Elevation Gain: {event.total_elev_gain.toLocaleString()}ft</p>
 
-            <div>
-                {event?.course_url ?
-                    <>
-                        Course Map
-                        <br />
-                        < CourseMap event={event} />
+                    {event.event_sports?.length > 1 ? <>
+                        <h4>Multi-Sport Event</h4>
+                        {event.event_sports?.map(es => {
+                            return <>
+                                <div>
+                                    {es.sport.name} | Distance: {es.distance}mi | Elevation Gain: {es.elev_gain}ft
+                                </div>
+                            </>
+                        })}
                     </>
-                    : ""}
-            </div>
+                        : ""}
+
+                    <p>{event.description}</p>
+
+
+
+                    <div>
+                        {event?.course_url ?
+                            <>
+                                Course Map
+                                <br />
+                                < CourseMap event={event} />
+                            </>
+                            : ""}
+                    </div>
+                </section>
+            </article>
         </>
     )
 }
--- src/components/profile/EditProfile.js
+++ src/components/profile/EditProfile.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { getAthlete, updateAthlete } from "./AthleteProvider";
+import "./EditProfile.css"
 
 export const EditProfile = () => {
     const [athlete, setAthlete] = useState({})
@@ -45,53 +46,56 @@ export const EditProfile = () => {
 
     return (
         <>
-            <h2>Edit Profile</h2>
 
-            <fieldset>
-                <label htmlFor="firstName"> First Name </label>
-                <input type="text" name="firstName" className="form-control" placeholder={athlete.firstName} required autoFocus onChange={handleUser} />
-            </fieldset>
-            <fieldset>
-                <label htmlFor="lastName"> Last Name </label>
-                <input type="text" name="lastName" placeholder={athlete.lastName} className="form-control" required onChange={handleUser} />
-            </fieldset>
-            <fieldset>
-                <label htmlFor="inputPassword">New Password </label>
-                <input type="password" name="password" className="form-control" placeholder="Password" required onChange={handleUser} />
-            </fieldset>
-            <fieldset>
-                <label htmlFor="verifyPassword">Verify New Password </label>
-                <input type="password" name="verifyPassword" className="form-control" placeholder="Verify password" required onChange={handleUser} />
-            </fieldset>
-            <fieldset>
-                <label htmlFor="bio">Bio </label>
-                <textarea name="bio" className="form-control" className="form-control" placeholder={athlete.bio} required onChange={handleUser} />
-            </fieldset>
-            <fieldset>
-                <label htmlFor="rhr">Resting Heart Rate </label>
-                <input type="number" name="rhr" className="form-control" placeholder={athlete.rhr} onChange={handleUser} /> bpm
-            </fieldset>
-            <fieldset>
-                <label htmlFor="vo2"> Vo2Max </label>
-                <input type="number" placeholder={athlete.VO2max} className="form-control" name="VO2max" step="0.01" onChange={handleUser} />
-            </fieldset>
-            <fieldset>
-                <label htmlFor="fluidLoss">Fluid loss</label>
-                <input type="number" placeholder={athlete.fluidLoss} className="form-control" name="fluidLoss" step="0.01" onChange={handleUser} /> L/hr
-            </fieldset>
-            <fieldset>
-                <label htmlFor="sodiumLoss">Sodium Loss</label>
-                <input type="number" placeholder={athlete.sodiumLoss} className="form-control" name="sodiumLoss" onChange={handleUser} /> mg/hr
-            </fieldset>
-            <fieldset>
-                <label htmlFor="weight">Weight </label>
-                <input type="number" placeholder={athlete.weight} className="form-control" name="weight" onChange={handleUser} /> lbs
-            </fieldset>
-            <fieldset style={{
-                textAlign: "center"
-            }}>
-                <button type="submit" onClick={handleUpdate}>Save</button>
-            </fieldset>
+            <h2 className="edit-h">Edit Profile</h2>
+            <div className="editProfile">
+                <fieldset>
+                    <label htmlFor="firstName"> First Name </label>
+                    <input type="text" name="firstName" className="form-control" placeholder={athlete.firstName} required autoFocus onChange={handleUser} />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="lastName"> Last Name </label>
+                    <input type="text" name="lastName" placeholder={athlete.lastName} className="form-control" required onChange={handleUser} />
+                </fieldset>
+                {/* <fieldset>
+                    <label htmlFor="inputPassword">New Password </label>
+                    <input type="password" name="password" className="form-control" placeholder="Password" required onChange={handleUser} />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="verifyPassword">Verify New Password </label>
+                    <input type="password" name="verifyPassword" className="form-control" placeholder="Verify password" required onChange={handleUser} />
+                </fieldset> */}
+                <fieldset>
+                    <label htmlFor="bio">Bio </label>
+                    <textarea name="bio" className="form-control" className="form-control" placeholder={athlete.bio} required onChange={handleUser} />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="rhr">Resting Heart Rate - bpm</label>
+                    <input type="number" name="rhr" className="form-control" placeholder={athlete.rhr} onChange={handleUser} />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="vo2"> Vo2Max - mL/kg/min</label>
+                    <input type="number" placeholder={athlete.VO2max} className="form-control" name="VO2max" step="0.01" onChange={handleUser} />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="fluidLoss">Fluid loss - L/hr</label>
+                    <input type="number" placeholder={athlete.fluidLoss} className="form-control" name="fluidLoss" step="0.01" onChange={handleUser} />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="sodiumLoss">Sodium Loss - mg/hr</label>
+                    <input type="number" placeholder={athlete.sodiumLoss} className="form-control" name="sodiumLoss" onChange={handleUser} />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="weight">Weight - lbs</label>
+                    <input type="number" placeholder={athlete.weight} className="form-control" name="weight" onChange={handleUser} />
+                </fieldset>
+                <fieldset style={{
+                    textAlign: "center"
+                }}>
+                    <button className="save-profile" onClick={handleUpdate}>Save</button>
+                    <button className="save-profile" onClick={()=>history.push("/profile")}>Cancel</button>
+                </fieldset>
+            </div>
         </>
     )
 }
