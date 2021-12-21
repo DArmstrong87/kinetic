8e2b01a6e355445d817c96b27aced7095de6e37e
DArmstrong87 <lenzflare@gmail.com>
2021-12-21 13:09:33 -0600

    added styling and button to view past events

--- src/components/eventCalendar/EventCalendarProvider.js
+++ src/components/eventCalendar/EventCalendarProvider.js
@@ -6,6 +6,14 @@ export const getAthleteEvents = () => {
     })
         .then(res => res.json())
 }
+export const getPastAthleteEvents = () => {
+    return fetch("https://kinetic--server.herokuapp.com/athleteevents?past", {
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("kinetic_token")}`
+        }
+    })
+        .then(res => res.json())
+}
 
 export const deleteEvent = (id) => {
     return fetch(`https://kinetic--server.herokuapp.com/events/${id}`, {
@@ -18,6 +26,15 @@ export const deleteEvent = (id) => {
 
 export const getOrganizerEvents = () => {
     return fetch("https://kinetic--server.herokuapp.com/organizerevents", {
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("kinetic_token")}`
+        }
+    })
+        .then(res => res.json())
+}
+
+export const getPastOrganizerEvents = () => {
+    return fetch("https://kinetic--server.herokuapp.com/organizerevents?past", {
         headers: {
             "Authorization": `Token ${localStorage.getItem("kinetic_token")}`
         }
