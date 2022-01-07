bd80a38b91975e5d23beb2a3505e0fe2a12b58a6
DArmstrong87 <lenzflare@gmail.com>
2022-01-07 13:30:56 -0600

    use effect handles checking for activities and achievements

--- src/components/profile/Achievements.js
+++ src/components/profile/Achievements.js
@@ -1,33 +1,87 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
+import { getActivities } from "../activities/ActivityProvider"
 import "./Achievements.css"
-import { achievementUnlocked, getAchievements, getBadges } from "./AchievementsProvider"
+import { achievementUnlocked, getAchievements, getBadges, validateAchievements } from "./AchievementsProvider"
 
 export const Achievements = () => {
     const [badges, setBadges] = useState([])
     const [achievements, setAchievements] = useState([])
+    const [newBadges, setNew] = useState([])
+    const [badgesSet, setBadgesSet] = useState(false)
+    const showAchievement = useRef()
 
     useEffect(
         () => {
-            getBadges().then(badges => setBadges(badges))
-            getAchievements().then(ach => setAchievements(ach))
+            const getThings = [
+                getBadges(),
+                getActivities(),
+                getAchievements()
+            ]
+            Promise.all(getThings)
+                .then(values => {
+                    setBadges(values[0])
+                    setAchievements(values[2])
+                    validateAchievements(values, newBadges, setNew)
+                }).then(
+                    () => {
+                        if (newBadges.length > 0) { setBadgesSet(true) }
+                        for (const badge of newBadges) {
+                            achievementUnlocked({ badgeId: badge.id })
+                        }
+                    }
+                )
+                .then(() => {
+                    if (newBadges.length > 0) {
+                        showAchievement.current.showModal()
+                    }
+                })
         }, []
     )
 
-    const validateActivities = () => {
-
-    }
+    console.log(newBadges)
 
     return (<>
+        <dialog className="achievementModal" ref={showAchievement}>
+            <h2 style={{ textAlign: "center", color: "white" }}>Achievements Unlocked!</h2>
+            <div className="newAchievements">
+                {newBadges?.map(badge => {
+                    return <div className="achievement-unlocked">
+                        {badge?.name} Unlocked!
+                        <div className="ach-img">
+                            <img src={badge?.complete_url} />
+                        </div>
+                        <p>{badge?.description}</p>
+                    </div>
+                })}
+            </div>
+            <div className="ach-button">
+                <button onClick={() => {
+                    showAchievement.current.close();
+                    getBadges().then(badges => setBadges(badges))
+                    getAchievements().then(a => setAchievements(a))
+                }}>
+                    Let's Go!
+                </button>
+            </div>
+        </dialog>
+
         <article className="achievements-container">
             <h1 className="achievements-title">Achievements</h1>
             <div className="badges">
                 {achievements.map(a => {
-                    return <div className="badge-unlocked"><img src={a.badge.complete_url} />
-                    </div>
+                    return <>
+                        <div className="badge-unlocked">
+                            <div>{a.badge.name}</div>
+                            <div className="badge-img-container"><img src={a.badge.complete_url} /></div>
+                        </div>
+                    </>
                 })}
                 {badges.map(badge => {
-                    return <div className="badge"><img src={badge.incomplete_url} />
-                    </div>
+                    return <>
+                        <div className="badge">
+                            <img src={badge.incomplete_url} />
+                        </div>
+                    </>
                 })}
             </div>
             View All Achievements
--- src/components/profile/AchievementsProvider.js
+++ src/components/profile/AchievementsProvider.js
@@ -27,3 +27,99 @@ export const getBadges = () => {
         .then(res => res.json())
 }
 
+
+// Achievement Validation
+export const validateAchievements = (values, newBadges, setNew) => {
+    const badges = values[0]
+    const activities = values[1]
+    const achievements = values[2]
+
+    const copy = newBadges
+    let firstUnlocked = first(badges, activities)
+    let cyclistUnlocked = cyclist(badges, activities, achievements)
+    let runnerUnlocked = runner(badges, activities, achievements)
+    let swimmerUnlocked = swimmer(badges, activities, achievements)
+    let hundredUnlocked = oneHundred(badges, activities, achievements)
+    const checkedAchievements = [cyclistUnlocked, runnerUnlocked, swimmerUnlocked, firstUnlocked, hundredUnlocked]
+    for (const checked of checkedAchievements) {
+        if (checked) {
+            copy.push(checked)
+        }
+    }
+    setNew(copy)
+}
+
+const first = (badges, activities, achievements) => {
+    const foundBadge = achievements?.find(a => {
+        return a?.badge?.name === "First"
+    })
+    if (foundBadge) {
+        return
+    }
+    else {
+        if (activities.length > 0) {
+            return badges.find(badge => badge.name === "First")
+        }
+    }
+}
+
+const cyclist = (badges, activities, achievements) => {
+    const foundBadge = achievements?.find(a => {
+        return a?.badge?.name === "Cyclist"
+    })
+    if (foundBadge) {
+        return
+    } else {
+        const foundActivity = activities?.find(a => {
+            return a.activity_sports.some(aS => {
+                return aS.sport.name === "Cycling"
+            })
+        })
+        if (foundActivity) { return badges.find(badge => badge.name === "Cyclist") }
+    }
+}
+
+const runner = (badges, activities, achievements) => {
+    const foundBadge = achievements?.find(a => {
+        return a?.badge?.name === "Runner"
+    })
+    if (foundBadge) {
+        return
+    } else {
+        const foundActivity = activities?.find(a => {
+            return a.activity_sports.some(aS => {
+                return aS.sport.name === "Running"
+            })
+        })
+        if (foundActivity) { return badges.find(badge => badge.name === "Runner") }
+    }
+}
+
+const swimmer = (badges, activities, achievements) => {
+    const foundBadge = achievements?.find(a => {
+        return a?.badge?.name === "Swimmer"
+    })
+    if (foundBadge) {
+        return
+    } else {
+        const foundActivity = activities?.find(a => {
+            return a.activity_sports.some(aS => {
+                return aS.sport.name === "Swimming"
+            })
+        })
+        if (foundActivity) { return badges.find(badge => badge.name === "Swimmer") }
+    }
+}
+
+const oneHundred = (badges, activities, achievements) => {
+    const foundBadge = achievements?.find(a => {
+        return a?.badge?.name === "100 Miles"
+    })
+    if (foundBadge) { return }
+    else {
+        const totalMi = activities.reduce((accumulator, a) => { return accumulator + a.total_distance }, 0)
+        if (totalMi >= 100) {
+            return badges.find(badge => badge.name === "100 Miles")
+        }
+    }
+}
